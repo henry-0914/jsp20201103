731a1ec57c3d1dfa6870ae431d5f8d461b0606c8
hany0 <hany7012@gmail.com>
2020-11-12 16:49:39 +0900

    jdbc

---
+++ WebContent/WEB-INF/sql/select/selectEx2.sql
@@ -0,0 +1,44 @@
+select * from employee;
+SELECT * FROM employee WHERE ename = 'SMITH';
+SELECT * FROM employee WHERE salary > 1000;
+SELECT * FROM employee WHERE commission < 500;
+SELECT * FORM employee WHERE salary >=3000;
+SELECT * FROM employee WHERE salary <=2000;
+SELECT * FROM employee WHERE ename <= 'G'; -- 문자는 작은 따옴표만 사용 --
+SELECT * FROM employee WHERE ename != 'SMITH';
+SELECT * FROM employee WHERE ename <> 'SMITH';
+SELECT * FROM employee WHERE ename ^= 'SMITH';
+SELECT * FROM employee WHERE hiredate = '1991/02/20';
+SELECT * FROM employee WHERE hiredate = '81/02/20';
+
+
+
+SELECT * FROM employee WHERE hiredate >='1981/01/01' AND salary > 3000;
+SELECT * FROM employee WHERE hiredate >='1981/01/01' OR salary > 3000;
+
+SELECT * FROM employee
+WHERE NOT  salary >3000;
+
+SELCET * FROM employee
+WHERE ename > 'A' AND salary > 3000 AND dno <> 30;
+
+SELECT * FROM employee
+WHERE salary >=3000 AND salary <= 5000;
+
+SELECT * FROM employee
+WHERE salary BETWEEN 3000 AND 5000;
+
+SELECT * FROM employee
+WHERE hiredate BETWEEN '1982/01/01' AND '1982/12/31';
+
+SELECT * FROM employee
+WHERE salary NOT BETWEEN 3000 AND  5000;
+
+SELECT * FROM employee
+WHERE dno = 10 OR dno = 20;
+
+SELECT * FROM employee
+WHERE dno IN (10, 20);
+
+SELECT * FROM  employee
+WHERE dno NOT IN (20);
